9489688565616a470da7e7a4fc3d4003e883ad7c
Khubab <khubab220@gmail.com>
2023-03-03 09:00:15 +0000

    Reformat base post file

--- forum/models/base_post.py
+++ forum/models/base_post.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import User
 
 
 class BasePost(models.Model):
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_posts')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   related_name='created_posts')
     created_on = models.DateTimeField(auto_now_add=True)
-    modifed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='modified_posts')
+    modifed_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   related_name='modified_posts')
     modified_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
     archive = models.BooleanField(default=False)
