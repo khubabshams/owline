7bd9ec892eab54d06633389463d1fe27d45518d1
Khubab <khubab220@gmail.com>
2023-03-03 07:40:40 +0000

    Fix login not working by add allauth account to settings

--- owline/settings.py
+++ owline/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.sites',
     'allauth',
+    'allauth.account',
     'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
