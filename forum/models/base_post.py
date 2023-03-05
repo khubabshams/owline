9ab5d35692a5f871e70e368d16b65b2c3261532e
Khubab <khubab220@gmail.com>
2023-03-05 08:08:03 +0000

    Fix typo of modified_by field

--- forum/models/base_post.py
+++ forum/models/base_post.py
@@ -6,8 +6,8 @@ class BasePost(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE,
                                    related_name='created_posts')
     created_on = models.DateTimeField(auto_now_add=True)
-    modifed_by = models.ForeignKey(User, on_delete=models.CASCADE,
-                                   related_name='modified_posts')
+    modified_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                    related_name='modified_posts')
     modified_on = models.DateTimeField(auto_now=True)
     body = models.TextField()
     archive = models.BooleanField(default=False)
