32a1585c3ce255c7c9d1bd0caf761eccb888b033
Khubab <khubab220@gmail.com>
2023-03-07 02:25:34 +0000

    Change url to be more meaningful, add update and create urls

--- forum/urls.py
+++ forum/urls.py
@@ -3,16 +3,21 @@ from django.urls import path
 
 urlpatterns = [
     path('', question.QuestionList.as_view(), name='home'),
-    path('create/', question.QuestionCreate.as_view(),
+    path('forum/', question.QuestionList.as_view(), name='forum'),
+    path('forum/create/', question.QuestionCreate.as_view(),
          name='question_create'),
-    path('<slug:slug>/', question.QuestionDetail.as_view(), name='question'),
-    path('<slug:slug>/update/', question.QuestionUpdate.as_view(),
+    path('forum/<slug:slug>/', question.QuestionDetail.as_view(),
+         name='question_details'),
+    path('forum/<slug:slug>/update/', question.QuestionUpdate.as_view(),
          name='question_update'),
-    path('<slug:slug>/delete/', question.QuestionDelete.as_view(),
+    path('forum/<slug:slug>/delete/', question.QuestionDelete.as_view(),
          name='question_delete'),
 
-    path('<slug:slug>/<pk>/update/', answer.AnswerUpdate.as_view(),
+
+    path('forum/<slug:slug>/create/', answer.AnswerCreate.as_view(),
+         name='answer_create'),
+    path('forum/<slug:slug>/<pk>/update/', answer.AnswerUpdate.as_view(),
          name='answer_update'),
-    path('<slug:slug>/<pk>/delete/', answer.AnswerDelete.as_view(),
+    path('forum/<slug:slug>/<pk>/delete/', answer.AnswerDelete.as_view(),
          name='answer_delete'),
 ]
