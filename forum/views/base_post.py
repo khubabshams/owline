2f01ca960a4d6bcd0590cc0a09d6544c82641e67
Khubab <khubab220@gmail.com>
2023-03-08 16:37:42 +0000

    Fix typo in answered flag check

--- forum/views/base_post.py
+++ forum/views/base_post.py
@@ -31,7 +31,8 @@ def action_sign_user_post_vote(user, slug, pk, increase=True):
 
 def action_accept_answer(user, slug, pk):
     answer = get_updated_object(slug=slug, pk=pk)
-    if not any([answer.accepted, answered, user == answer.created_by]) and\
+    if not any([answer.accepted, answer.related_question.answered,
+                user == answer.created_by]) and\
             user == answer.related_question.created_by:
         answer.related_question.answered = True
         answer.accepted = True
