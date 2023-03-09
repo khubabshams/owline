5a707353d2e75c40d27b5b3f1ab8ff041379333a
Khubab <khubab220@gmail.com>
2023-03-09 07:30:59 +0000

    Message model and bits create view template

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
+                                   related_name='messages')
+    created_on = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    archive = models.BooleanField(default=False)
