2267335e9ce36cf9e8cb27e15102b52eb0c4b66d
Khubab <khubab220@gmail.com>
2023-03-23 08:40:31 +0000

    Add answer acceptance tests of it's cases

--- forum/tests/test_base_post_views.py
+++ forum/tests/test_base_post_views.py
@@ -287,15 +287,133 @@ class TestBasePostViews(TestViews):
             reverse('answer_accept', kwargs={'slug': self.Question1.slug,
                                              'pk': self.Answer1.id}), {})
         self.Answer1.refresh_from_db()
+        self.Question1.refresh_from_db()
         self.RegularUser.refresh_from_db()
         messages = [m.message for m in
                     get_messages(answer_accept_response.wsgi_request)]
+        self.assertTrue(self.Answer1.accepted, "Answer should be accpeted")
+        self.assertTrue(self.Question1.answered,
+                        "Question should be answered after acceptance")
         self.assertEqual(self.Answer1.votes, 3,
                          "Acceptance score should be counted")
         self.assertEqual(self.RegularUser.profile.score, 3,
                          "Acceptance should affect user's score")
         self.assertIn('an Answer has been accepted successfully.', messages,
                       "User should be told that his acceptance succeed")
+        self.assertEqual(answer_accept_response.url,
+                         f'/forum/{self.Question1.slug}/',
+                         "User must be redirect to related question")
+
+    def test_question_owner_accept_his_own_answer_success(self):
+        question_owner_answer = Answer.objects.\
+            create(created_by=self.AdminUser, modified_by=self.AdminUser,
+                   body='BODY', related_question=self.Question1)
+        question_owner_answer_accept_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': question_owner_answer.id}),
+            {})
+        question_owner_answer.refresh_from_db()
+        self.Question1.refresh_from_db()
+        self.AdminUser.refresh_from_db()
+        messages = [m.message for m in
+                    get_messages(question_owner_answer_accept_response.
+                                 wsgi_request)]
+        self.assertTrue(question_owner_answer.accepted,
+                        "Answer should be accpeted")
+        self.assertTrue(self.Question1.answered,
+                        "Question should be answered after acceptance")
+        self.assertEqual(question_owner_answer.votes, 0,
+                         "Question's owner acceptance of his own answer should"
+                         " not be counted")
+        self.assertEqual(self.AdminUser.profile.score, 0,
+                         "Question's owner acceptance of his own answer should"
+                         " not affect his score")
+        self.assertIn('an Answer has been accepted successfully.', messages,
+                      "User should be told that his acceptance succeed")
+        self.assertEqual(question_owner_answer_accept_response.url,
+                         f'/forum/{self.Question1.slug}/',
+                         "User must be redirect to related question")
+
+    def test_answer_accept_answered_question(self):
+        to_be_accepted_answer = Answer.objects.\
+            create(created_by=self.AdminUser, modified_by=self.AdminUser,
+                   body='BODY', related_question=self.Question1)
+        answer_accept_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': to_be_accepted_answer.id}),
+            {})
+        accept_another_answer_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': self.Answer1.id}), {})
+        self.Answer1.refresh_from_db()
+        self.RegularUser.refresh_from_db()
+        messages = [m.message for m in
+                    get_messages(accept_another_answer_response.
+                                 wsgi_request)]
+        self.assertFalse(self.Answer1.accepted,
+                         "Answer should not be accpeted")
+        self.assertEqual(self.Answer1.votes, 0,
+                         "Accpetance of answered question should not be"
+                         " counted")
+        self.assertEqual(self.RegularUser.profile.score, 0,
+                         "Accpetance score of answered question should not be"
+                         " counted")
+        self.assertIn('Question is already has an accepted answer.',
+                      messages, "User must be warned for already accepted "
+                      "answer")
+        self.assertEqual(accept_another_answer_response.url,
+                         f'/forum/{self.Question1.slug}/',
+                         "User must be redirect to related question")
+
+    def test_answer_accept_twice(self):
+        answer_first_accept_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': self.Answer1.id}), {})
+        answer_second_accept_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': self.Answer1.id}), {})
+        self.Answer1.refresh_from_db()
+        self.RegularUser.refresh_from_db()
+        messages = [m.message for m in
+                    get_messages(answer_second_accept_response.
+                                 wsgi_request)]
+        self.assertEqual(self.Answer1.votes, 3,
+                         "Only first acceptance should be counted")
+        self.assertEqual(self.RegularUser.profile.score, 3,
+                         "Only first acceptance score should be counted")
+        self.assertIn('Answer is already accepted before.',
+                      messages, "User must be warned for already accepted "
+                      "answer")
+        self.assertEqual(answer_second_accept_response.url,
+                         f'/forum/{self.Question1.slug}/',
+                         "User must be redirect to related question")
+
+    def test_answer_accept_by_not_question_owner(self):
+        self.client.logout()
+        self.client.login(username=self.RegularUser.username,
+                          password=PASSWORD)
+        answer_accept_response = self.client.post(
+            reverse('answer_accept', kwargs={'slug': self.Question1.slug,
+                                             'pk': self.Answer1.id}), {})
+        self.Answer1.refresh_from_db()
+        self.Question1.refresh_from_db()
+        self.RegularUser.refresh_from_db()
+        messages = [m.message for m in
+                    get_messages(answer_accept_response.wsgi_request)]
+        self.assertFalse(self.Answer1.accepted,
+                         "Answer should not be accpeted")
+        self.assertFalse(self.Question1.answered,
+                         "Question should not be answered")
+        self.assertEqual(self.Answer1.votes, 0,
+                         "Not question's owner acceptance score should not be"
+                         " counted")
+        self.assertEqual(self.RegularUser.profile.score, 0,
+                         "Not question's owner acceptance should not affect"
+                         " user's score")
+        self.assertIn('You can only accept answers to your own questions.',
+                      messages,
+                      "User should be told that he can accept only his own"
+                      " questions' answers")
         self.assertEqual(answer_accept_response.url,
                          f'/forum/{self.Question1.slug}/',
                          "User must be redirect to related question")
