6353405af5f9f78511277add3851961e18c54133
Khubab <khubab220@gmail.com>
2023-03-24 23:54:02 +0000

    Add test for home app profile and user

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from django.urls import reverse
+from .models import Profile
+
+
+class TestModels(TestCase):
+
+    def setUp(self):
+        self.NewUser = User.objects.create_superuser('username',
+                                                     'useremail@owline.com',
+                                                     'PA$$WoRd@23')
+
+    # Test Profile model
+    def test_user_has_profile(self):
+        self.assertTrue(self.NewUser.profile,
+                        "User creation should create a related profile")
+
+    def test_profile_defaults(self):
+        profile = self.NewUser.profile
+        self.assertEqual(f'username\n [0 Points]', profile.__str__())
+        self.assertEqual(profile.score, 0,
+                         "Score on user profile upon creation shoul be zero")
